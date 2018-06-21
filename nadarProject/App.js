1274ef986f9eca032e70e3284bfc89a1136ed638
Alex Bui <alxbui14@gmail.com>
2018-06-21 20:11:11 +0200

    creation page suite à la page home

--- nadarProject/App.js
+++ nadarProject/App.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import { createStackNavigator } from 'react-navigation'
+import Home from './components/Home.js'
 import Slide5 from './components/Slide5.js'
 import Slide4 from './components/Slide4.js'
 import Slide3 from './components/Slide3.js'
 import Slide2 from './components/Slide2.js'
 import Slide1 from './components/Slide1.js'
+import Jeu1 from './components/Jeu1.js'
+import Jeu2 from './components/Jeu2.js'
+import Exposition1 from './components/Exposition1'
 import { Font } from 'expo'
 import { Text } from 'react-native'
 
 const RootStack = createStackNavigator(
   {
+    Home: Home,
     Slide1: Slide1,
     Slide2: Slide2,
     Slide3: Slide3,
     Slide4: Slide4,
     Slide5: Slide5,
+    Exposition1: Exposition1,
+    Jeu1: Jeu1,
+    Jeu2: Jeu2
   },
   {
-    initialRouteName: 'Slide5',
+    initialRouteName: 'Jeu1',
     headerMode: 'none',
   }
 )
--- nadarProject/components/Input.js
+++ nadarProject/components/Input.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { TextInput } from react-native
 import styles from '../styles.js'
 
-export const Input = (props) => 
+export const Input = ({placeholderText, placeholderType, newValue }) => 
   <TextInput
     editable = {true}
     maxLength = {40}
     style={ [styles.textInput, {marginTop: 50, color: 'white'}] }
+    placeholder={placeholderText}
+    placeholderTextColor = 'white'
+    onChangeText={(text) => this.setState({text[placeholderText]})}
+    value={newValue} 
   />
 
